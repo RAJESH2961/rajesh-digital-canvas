2af3c61f58977a781a461bfc14439aba994c787b
Rajesh <105571840+RAJESH2961@users.noreply.github.com>
2025-06-19 10:58:32 +0530

    Stock Prediction Live link added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -123,7 +123,7 @@ const Index = () => {
     description: "Real-time stock prediction using machine learning with interactive frontend.",
     techStack: ["React", "Django", "MachineLearning", "Postman", "SQL", "JWT Authentication", "Three.js"],
     githubUrl: "https://github.com/RAJESH2961/stock-prediction-portal.git",
-    liveUrl: "https://github.com/RAJESH2961/stock-prediction-portal.git",
+    liveUrl: "https://stock-prediction-portal-magn.onrender.com",
     featured: true
   },
   {
